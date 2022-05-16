0ac27f8fb30b4820ecc9736c0e70743fda482c57
shivamtg <shivamtg1376@gmail.com>
2022-05-16 11:32:07 +0200

    add multiplier btn

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 
 
 (() => {
-    let counter = 0
+    let counter = 0;
     let countertag = document.getElementById('countNum');
     let cookie = document.getElementById('cookie');
+    let clickMultBtn = document.getElementById('cookieMultiplier');
+    var mult = 1 ;
     console.log(cookie);
     cookie.addEventListener('click', () =>{
-        counter ++;
-        console.log(counter);
+       counter += mult;
         countertag.innerHTML = counter ;
     })
+
+    clickMultBtn.addEventListener('click' , ()=>{
+        if (counter>= 25){
+            mult++;
+            document.getElementById('multCount').innerText = mult;
+            counter = counter - 25;
+            countertag.innerHTML = counter;
+        }else {
+            window.alert('OOPS!! You don\'t have enough Cookies!');
+        }
+    })
+
 })();
