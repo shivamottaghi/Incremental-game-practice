67b37c4d425ca6922cac04745547d96b614a95a3
shivamtg <shivamtg1376@gmail.com>
2022-05-16 16:01:10 +0200

    add auto clicker

--- script.js
+++ script.js
@@ -7,8 +7,13 @@
     let clickMultBtn = document.getElementById('cookieMultiplier');
     let multCostTag = document.getElementById('multCost');
     let multCountTag = document.getElementById('multCount');
+    let autoClickBtn = document.getElementById('cookieAutoClicker');
+    let autoCostTag = document.getElementById('autoClickCost');
+    let autoCountTag = document.getElementById('autoCount');
     let mult = 1 ;
     let multCost = 25;
+    let auto = 0;
+    let autoCost = 150;
     //event listener for cookie
     cookie.addEventListener('click', () =>{
        counter += mult;
@@ -27,5 +32,26 @@
             window.alert('OOPS!! You don\'t have enough Cookies!');
         }
     })
+    //event listener for auto clicker
+    autoClickBtn.addEventListener('click', ()=>{
+        if (counter>= autoCost){
+            auto++;
+            if (auto == 1){
+                autoCountTag.innerHTML= '1 cookie';
+            }else {
+                autoCountTag.innerHTML = `${auto} cookies`;
+            }
+            counter = counter - autoCost;
+            countertag.innerHTML = counter;
+            autoCost+= Math.floor(autoCost/2);
+            autoCostTag.innerHTML = autoCost;
+        }else {
+            window.alert('OOPS!! You don\'t have enough Cookies!');
+        }
+    })
 
+    setInterval(()=>{
+        counter+= auto;
+        countertag.innerHTML = counter;
+    }, 1000)
 })();
